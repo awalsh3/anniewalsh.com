e5d184443494b5cd44a816c17291a728938fa4a7
Annie Walsh <annie@anyonething.co.uk>
2022-02-04 12:55:45 +0000

    adds images, tailwind config, landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'about', to: 'pages#about'
+  get 'garden', to: 'pages#garden'
 end
