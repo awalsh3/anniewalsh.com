f6edd0847456069f977909fa865e1275e0decf3a
Annie Walsh <annie@anyonething.co.uk>
2022-04-20 14:20:30 +0100

    attempt to add video

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../builds
+// = link_tree ../builds
 
-//= link tailwind.css
+= link nowvideo.mp4
