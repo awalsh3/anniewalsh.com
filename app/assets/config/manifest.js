d85a28bc4aa3a0c3bc68a38f8209bad3c07ea852
Annie Walsh <annie@anyonething.co.uk>
2022-04-20 16:16:07 +0100

    now video and text about what I'm up to

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-// = link_tree ../builds
+//= link_tree ../builds
 
-= link nowvideo.mp4
+//= link nowvideo.mp4
